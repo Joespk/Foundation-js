51076bf07d3975a1d7af4d2b0f358c50ca2f0c17
Joespk <supakritjoe@gmail.com>
2023-09-12 16:13:57 +0700

    add loop

---
+++ LoopExercise.js
@@ -0,0 +1,23 @@
+// * Exercise:
+// * Write a code that will loop from 0 to 15. For each loop, it will check if the current index number is odd or even, and display a message to the console.
+
+// * Example Output:
+// * > 0 is even
+// * > 1 is odd
+// * > 2 is even
+
+// * > 15 is odd
+
+function oddEven() {
+  let num = 0;
+  while (num <= 15) {
+    if (num % 2 === 0) {
+      console.log(num, "is even");
+    } else {
+      console.log(num, "is odd");
+    }
+    i++;
+  }
+}
+
+oddEven();
