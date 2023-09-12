70af236407250af6f18efc10dff5e789b987d5a4
Joespk <supakritjoe@gmail.com>
2023-09-12 15:31:13 +0700

    'Loop'

--- LoopClassroom.js
+++ LoopClassroom.js
@@ -1,3 +1,4 @@
+//*normal Loop
 function normalLoop() {
   for (let i = 1; i <= 5; i++) {
     console.log(i);
@@ -8,24 +9,39 @@ function normalLoop() {
   }
 }
 
-//normal Loop
-
+//*reverse Loop
 function reverseLoop() {
   for (let i = 3; i >= 0; i++) {
     console.log(i);
   }
 }
 
-//reverse Loop
-
+//*Loop through array
 function loopThroughArray() {
   const tripLocation = ["London", "Bali", "Amsterdam", "Paris"];
+  /*for (let i = 0; i < tripLocation.length; i++) {
+    console.log(tripLocation[i]);
+  }/*
+
+  //*Reverse Loop through array
+  /*for (let i = tripLocation.length - 1; i >= 0; i--) {
+    console.log(tripLocation[i]);
+  }*/
+
+  //*Break Keyword
   for (let i = 0; i < tripLocation.length; i++) {
+    if (tripLocation[i] === "Bali") {
+      break; //คือเมื่อเงื่อนไขให้หยุดทำLoopทันที
+    }
     console.log(tripLocation[i]);
   }
 
-  //Reverse Loop through array
-  for (let i = tripLocation.length - 1; i >= 0; i--) {
+  //Continue keyword
+  for (let i = 0; i < tripLocation.length; i++) {
+    if (tripLocation[i] === "Bali") {
+      continue; //คือเมื่อเงื่อนไขให้หยุดทำLoop แล้วเริ่มLoopถัดไป
+      //EX: เจอBali ที่Loop 1 แล้วจะไม่มีการprintค่าLoop1 แต่จะมีการทำLoop 2,3 แล้วPrint ต่อ
+    }
     console.log(tripLocation[i]);
   }
 }
