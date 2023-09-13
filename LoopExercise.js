682d2aa3e0a980db6c84a789f76fb0c747e7068a
Joespk <supakritjoe@gmail.com>
2023-09-13 13:07:05 +0700

    'Loopclasswork'

--- LoopExercise.js
+++ LoopExercise.js
@@ -16,8 +16,20 @@ function oddEven() {
     } else {
       console.log(num, "is odd");
     }
-    i++;
+
+    num++;
   }
 }
 
 oddEven();
+// * Using While Loop
+// let i = 0
+// while (i <= 15) {
+//   if (i % 2 === 0) {
+//     console.log(i, 'is even')
+//   } else {
+//     console.log(i, 'is odd')
+//   }
+
+//   i++
+// }
