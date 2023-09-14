3515386554cc60e70b800a3ef7a3986827fb3ca4
Joespk <supakritjoe@gmail.com>
2023-09-14 17:16:32 +0700

    aaa

--- Foundetion-js/Foundetion-exercise3.js
+++
@@ -1,8 +0,0 @@
-/*3.Write a function isMember(mem, arr)
-
-isMember(mem, arr) returns a boolean indicating whether mem is a member of arr
-
-Do not use Array helper methods - use a simple for loop
-
-isMember(5, [1, 3, 7, 12]) // false
-isMember('john', ['jane', 'jim', 'john']) // true*/
