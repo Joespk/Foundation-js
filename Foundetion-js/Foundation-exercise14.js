9f0d9f18c4db25f0ea7a53868d0cbdecc6553f13
Joespk <supakritjoe@gmail.com>
2023-09-15 14:21:35 +0700

    14

--- Foundetion-js/Foundation-exercise14.js
+++ Foundetion-js/Foundation-exercise14.js
@@ -8,25 +8,24 @@ mid(['john']) // ["john"]
 mid(['foo', 'bar', 'baz']) // ["bar"]
 mid([1, 2, 3, 4]) // [2, 3] */
 
-function mid(arr) {
-  const length = arr.length;
+//Answer
+//not even
+const mid = (arr) => {
+  if (arr.length === 1) return arr;
 
-  if (length === 0) {
-    return []; // Empty array if input array is empty
+  const middlePosition = Math.floor(arr.length / 2);
+
+  if (arr.length % 2 === 0) {
+    const elem1 = arr[middlePosition - 1];
+    const elem2 = arr[middlePosition];
+
+    return [elem1, elem2];
   }
 
-  if (length % 2 === 0) {
-    // If the array length is even, return the two middle elements
-    const middleIndex1 = length / 2 - 1;
-    const middleIndex2 = length / 2;
-    return [arr[middleIndex1], arr[middleIndex2]];
-  } else {
-    // If the array length is odd, return the single middle element
-    const middleIndex = Math.floor(length / 2);
-    return [arr[middleIndex]];
-  }
-}
+  return [arr[middlePosition]];
+};
 
+//Test case
 console.log(mid(["john"])); // ["john"]
 console.log(mid(["foo", "bar", "baz"])); // ["bar"]
 console.log(mid([1, 2, 3, 4])); // [2, 3]
